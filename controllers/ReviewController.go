bb56a82215da68ab23dd61faa60589342984de70
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-10-25 00:40:42 +0700

    add & implement service

--- controllers/ReviewController.go
+++ controllers/ReviewController.go
@@ -1,27 +1,53 @@
 package controllers
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/ihksanghazi/backend-marketplace/model/web"
+	"github.com/ihksanghazi/backend-marketplace/services"
+	"github.com/ihksanghazi/backend-marketplace/utils"
 )
 
 type ReviewController interface {
 	Create(c *gin.Context)
 }
 
-type reviewControllerImpl struct{}
+type reviewControllerImpl struct {
+	service services.ReviewService
+}
 
-func NewReviewController() ReviewController {
-	return &reviewControllerImpl{}
+func NewReviewController(service services.ReviewService) ReviewController {
+	return &reviewControllerImpl{
+		service: service,
+	}
 }
 
 func (r *reviewControllerImpl) Create(c *gin.Context) {
 	productId := c.Param("productId")
+
+	refreshToken, err := c.Cookie("tkn_ck")
+	if err != nil {
+		c.JSON(401, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	claims, err := utils.ParsingToken(refreshToken, os.Getenv("REFRESH_TOKEN"))
+	if err != nil {
+		c.JSON(401, gin.H{"error": "Unauthorized"})
+		return
+	}
+
 	var req web.CreateReviewRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(200, gin.H{"id": productId, "req": req})
+	if err := r.service.Create(claims.ID, productId, req); err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{"msg": "Success Create Product Review"})
 }
