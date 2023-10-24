435833090ac659e7a1a1bd769fcb0ed4a80df580
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-10-25 02:08:17 +0700

    add service & implement

--- controllers/ReviewController.go
+++ controllers/ReviewController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/ihksanghazi/backend-marketplace/model/web"
@@ -55,6 +56,33 @@ func (r *reviewControllerImpl) Create(c *gin.Context) {
 
 func (r *reviewControllerImpl) Get(c *gin.Context) {
 	productId := c.Param("productId")
+	page := c.DefaultQuery("page", "1")
+	limit := c.DefaultQuery("limit", "10")
 
-	c.JSON(200, gin.H{"id": productId})
+	page_int, err := strconv.Atoi(page)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	limit_int, err := strconv.Atoi(limit)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	result, totalPage, err := r.service.Get(productId, page_int, limit_int)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	response := web.Pagination{
+		Code:        200,
+		Status:      "OK",
+		CurrentPage: page,
+		TotalPage:   totalPage,
+		Data:        result,
+	}
+
+	c.JSON(200, response)
 }
--- model/web/Review.go
+++ model/web/Review.go
@@ -1,5 +1,22 @@
 package web
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type CreateReviewRequest struct {
 	Comment string `json:"comment" binding:"required"`
+}
+
+type GetReviewResponse struct {
+	Id        uuid.UUID `json:"id"`
+	Comment   string    `json:"comment"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (r *GetReviewResponse) TableName() string {
+	return "reviews"
 }
